3b00a0248368cb336c01dbf0906464ff37455e16
Rob Miller <cowboysfn007@gmail.com>
2018-04-19 20:00:56 -0700

    actually saved my scene this time

--- Assets/FlagCapture.cs
+++ Assets/FlagCapture.cs
@@ -7,6 +7,9 @@ public class FlagCapture : MonoBehaviour {
     void OnTriggerEnter(Collider hit) {
         if (hit.gameObject.tag == "Player") {
             Application.Quit();
+            if (UnityEditor.EditorApplication.isPlaying == true) {
+                Debug.Log("Player Wins!!!");
+            }
         }
     }
 }
