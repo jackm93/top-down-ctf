73d3f05712858569f24f732a15b735ebe5ac0dd0
Becca Little <beccannlittle@gmail.com>
2018-04-19 21:01:10 -0700

    removing comments, serializing public variables, and turning player/camera into prefabs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,22 +4,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-    //public GameObject player to hold the player Object
-    public GameObject player;
+	[SerializeField]
+    GameObject player;
 
-    //private Vector3 offset to find initial difference between player Object and camera Object
     private Vector3 offset;
 
-	//used to initialize datamembers
 	void Start () {
-
-        //set offset = to camera position - player position
         offset = transform.position - player.transform.position;
 	}
-	
-    //called every frame and guaranteed to run after everything processed in Update
-	void LateUpdate () {
 
+	void LateUpdate () {
         transform.position = player.transform.position + offset;
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    //private Rigidbody playerRB to control player movement
+	[SerializeField]
+	float speed;
+	
     private Rigidbody playerRB;
 
-    //publc float speed to modify the speed of the player
-    public float speed;
-
     void Start() {
-
-        //set the Rigidbody equal to the Rigidbody component attached to the player
         playerRB = GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate() {
-
-        //declare two floats horiz, vert and set equal to respective Input axis
+		
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        //declare Vector3 movement and instantiate with new Vector 3 x = horiz, y = 0, z = vert
         Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
-        //call AddForce for the Rigidbody by passing movement * speed
         playerRB.AddForce(movement * speed);
             
     }
