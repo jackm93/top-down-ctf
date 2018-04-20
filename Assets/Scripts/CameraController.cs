f5de8042fbdcfa45d5ebcf88ecfae56a76a170ea
Sergeant-Jaeger <30932450+Sergeant-Jaeger@users.noreply.github.com>
2018-04-19 21:12:22 -0500

    Adds a player sphere with movement control and a fixed camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    //public GameObject player to hold the player Object
+    public GameObject player;
+
+    //private Vector3 offset to find initial difference between player Object and camera Object
+    private Vector3 offset;
+
+	//used to initialize datamembers
+	void Start () {
+
+        //set offset = to camera position - player position
+        offset = transform.position - player.transform.position;
+	}
+	
+    //called every frame and guaranteed to run after everything processed in Update
+	void LateUpdate () {
+
+        transform.position = player.transform.position + offset;
+	}
+}
