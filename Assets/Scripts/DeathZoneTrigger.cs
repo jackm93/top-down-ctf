bd1b3d32c2f2803f5ced35cf461850843ac954c8
Rob Miller <cowboysfn007@gmail.com>
2018-04-19 20:46:49 -0700

    added a death zone below the map

---
+++ Assets/Scripts/DeathZoneTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZoneTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            //Respawn Player
+            Debug.Log("Player Loses!!!");
+        }
+    }
+}
