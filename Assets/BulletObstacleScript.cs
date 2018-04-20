f9b242e6972655980cd02db552ea3783326189b3
Sergeant-Jaeger <30932450+Sergeant-Jaeger@users.noreply.github.com>
2018-04-20 18:12:50 -0500

    Creates a bullet obstacle

---
+++ Assets/BulletObstacleScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletObstacleScript : MonoBehaviour {
+
+    public GameObject barrel;
+
+    public GameObject bulletPrefab;
+
+    public Transform bulletSpawn;
+
+    private Vector3 trajectory;
+
+	// Use this for initialization
+	void Start () {
+
+        InvokeRepeating("Fire", 2.0f, 2.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Fire() {
+
+        var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward;
+
+        Destroy(bullet, 2.0f);
+    }
+}
