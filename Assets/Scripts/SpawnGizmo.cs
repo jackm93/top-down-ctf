b6cc245fbf1a4409a514904d6e199e8c72eab132
Jackm1993 <31322883+Jackm1993@users.noreply.github.com>
2018-04-21 10:57:29 -0500

    SpawnGizmo Script
    Added script to show gizmos on spawn. Updated player spawn.

---
+++ Assets/Scripts/SpawnGizmo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGizmo : MonoBehaviour {
+	[SerializeField]
+	private Mesh spawnMesh;
+
+	[SerializeField]
+	private Color color;
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = color;
+		Gizmos.DrawMesh(spawnMesh, 0, transform.position, transform.rotation, new Vector3(1,0.2f,1));
+	}
+}
